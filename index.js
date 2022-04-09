e42b018926d72a78e13ced2fde0106e2a8a1ae7c
umit sarisu <umitsarisu@gmail.com>
2022-04-09 15:11:33 +0300

    for github pages edits

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ function pageTransition(x) {
     $("iframe").addClass("none");
     $("section").addClass("none");
     if (x == 0) { $("section").removeClass("none"); }
-    else if (x == 1) { $("iframe[src='./pages/copy-paste/copy-paste.html']").removeClass("none"); }
-    else if (x == 2) { $("iframe[src='./pages/spare-parts/spare-parts.html']").removeClass("none"); }
-    else if (x == 3) { $("iframe[src='./pages/cari-kodlar/cari-kodlar.html']").removeClass("none"); }
-    else if (x == 4) { $("iframe[src='./pages/forms/device-register-form.html']").removeClass("none"); }
-    else if (x == 5) { $("iframe[src='./pages/forms/run-in-form.html']").removeClass("none"); }
+    else if (x == 1) { $("iframe[src='CopyPaste/pages/copy-paste/copy-paste.html']").removeClass("none"); }
+    else if (x == 2) { $("iframe[src='CopyPaste/pages/spare-parts/spare-parts.html']").removeClass("none"); }
+    else if (x == 3) { $("iframe[src='CopyPaste/pages/cari-kodlar/cari-kodlar.html']").removeClass("none"); }
+    else if (x == 4) { $("iframe[src='CopyPaste/pages/forms/device-register-form.html']").removeClass("none"); }
+    else if (x == 5) { $("iframe[src='CopyPaste/pages/forms/run-in-form.html']").removeClass("none"); }
 }
